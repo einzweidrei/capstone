084240eca34ea7b1ec2624b2473c6c75b700ece8
yuko <einzweidrei2@gmail.com>
2017-04-29 15:57:11 +0700

    Up

--- svcap.js
+++ svcap.js
@@ -24,7 +24,7 @@ mongoose.connect(mongodburi);
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://yukoserver.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
