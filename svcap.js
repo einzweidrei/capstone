5b15e77cc96d86e26627e38e985c067767901c59
yuko <einzweidrei2@gmail.com>
2017-04-29 16:23:11 +0700

    Update CORS

--- svcap.js
+++ svcap.js
@@ -5,6 +5,7 @@ var http = require('http').Server(app);
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var router = express.Router();
+var cors = require('cors');
 var io = require('socket.io')(http);
 
 // var SessionStore = require("session-mongoose")(express);
@@ -21,25 +22,27 @@ mongoose.connect(mongodburi);
 //     connection: mongoose.connection });
 
 // Add headers
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:6969');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:6969');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
 
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
+
+app.user(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyparser.urlencoded({
