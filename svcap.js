7109c623884e22c5f670696eff53271c336ffbeb
einzweidrei <einzweidrei2@gmail.com>
2017-04-29 18:49:10 +0700

    Update svcap.js

--- svcap.js
+++ svcap.js
@@ -1,13 +1,13 @@
 // params
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
+// var http = require('http').Server(app);
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var router = express.Router();
-var cors = require('cors');
-var SocketServer = require('ws').Server;
-var io = require('socket.io')(http);
+// var cors = require('cors');
+// var SocketServer = require('ws').Server;
+// var io = require('socket.io')(http);
 // const path = require('path');
 
 
@@ -25,32 +25,32 @@ mongoose.connect(mongodburi);
 //     connection: mongoose.connection });
 
 // Add headers
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//     // Website you wish to allow to connect
-//     res.setHeader('Access-Control-Allow-Origin', '*');
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
-//     // Request methods you wish to allow
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//     // Request headers you wish to allow
-//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
 
-//     // Set to true if you need the website to include cookies in the requests sent
-//     // to the API (e.g. in case you use sessions)
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
 
-//     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
-//     // Pass to next layer of middleware
-//     next();
-// });
+    // Pass to next layer of middleware
+    next();
+});
 
-var corsOptions = {
-    origin: 'https://yukoserver.herokuapp.com',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//     origin: 'https://yukoserver.herokuapp.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyparser.urlencoded({
@@ -82,53 +82,53 @@ app.use('/topic', require('./routes/topic.router'));
 //     console.log('listening on 6969 <3')
 // });
 
-var PORT = process.env.PORT || 3000;
+// var PORT = process.env.PORT || 3000;
 // const INDEX = path.join(__dirname, 'index.html');
 
-http.listen(PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('listening on 6969 <3')
 });
 
-var wss = new SocketServer({
-    perMessageDeflate: false,
-    port: 6969
-});
+// var wss = new SocketServer({
+//     perMessageDeflate: false,
+//     port: 6969
+// });
 
-wss.on('connection', (ws) => {
-    console.log('Client connected');
-    ws.on('close', () => console.log('Client disconnected'));
-});
+// wss.on('connection', (ws) => {
+//     console.log('Client connected');
+//     ws.on('close', () => console.log('Client disconnected'));
+// });
 
-setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-    });
-}, 1000);
+// setInterval(() => {
+//     wss.clients.forEach((client) => {
+//         client.send(new Date().toTimeString());
+//     });
+// }, 1000);
 
 // io.configure(function () {
 //     io.set("transports", ["xhr-polling"]);
 //     io.set("polling duration", 10);
 // });
 
-io.on('connection', function (socket) {
-    console.log('a user connected');
+// io.on('connection', function (socket) {
+//     console.log('a user connected');
 
-    socket.on('subscribe', function (room) {
-        console.log('joining room', room);
-        socket.join(room);
-    })
+//     socket.on('subscribe', function (room) {
+//         console.log('joining room', room);
+//         socket.join(room);
+//     })
 
-    socket.on('unsubscribe', function (room) {
-        console.log('leaving room', room);
-        socket.leave(room);
-    })
+//     socket.on('unsubscribe', function (room) {
+//         console.log('leaving room', room);
+//         socket.leave(room);
+//     })
 
-    socket.on('send', function (data) {
-        console.log('sending message');
-        io.sockets.in(data.room).emit('message', data.message);
-    });
+//     socket.on('send', function (data) {
+//         console.log('sending message');
+//         io.sockets.in(data.room).emit('message', data.message);
+//     });
 
-    socket.on('disconnect', function () {
-        console.log('user disconnected');
-    });
-});
+//     socket.on('disconnect', function () {
+//         console.log('user disconnected');
+//     });
+// });
